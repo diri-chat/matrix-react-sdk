4a6f615092752017b2d07d1d131d7813ade37e05
Bruno Windels <brunow@matrix.org>
2019-02-22 12:22:36 +0100

    rename all instances of permalinkCreator

--- src/components/views/rooms/EventTile.js
+++ src/components/views/rooms/EventTile.js
@@ -540,8 +540,8 @@ module.exports = withMatrixClient(React.createClass({
         });
 
         let permalink = "#";
-        if (this.props.permaLinkCreator) {
-            permalink = this.props.permaLinkCreator.forEvent(this.props.mxEvent.getId());
+        if (this.props.permalinkCreator) {
+            permalink = this.props.permalinkCreator.forEvent(this.props.mxEvent.getId());
         }
 
         const readAvatars = this.getReadAvatars();
